20d2e4723af2de9105981ccd13036362e7e7f8c1
Alex <alex.beciana@gmail.com>
2020-12-10 13:10:50 -0500

    Updated SEO description

--- siteConfig.js
+++ siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "Alex Beciana",
   shortName: "",
   description:
-    "An enthusiastic freelance full stack developer looking for new opportunities!",
+    "Enthusiastic full-stack web developer with an entrepreneurial spirit with experience in digital marketing and community management and leading a profitable startup.",
   url: "https://www.alexbeciana.com/", // Domain of your site without prefix!
   prefix: "/",
   author: "Alex Beciana",
